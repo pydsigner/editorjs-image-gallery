207e65d09c7645a739d848eb8e3a700ec2f2a15e
Daniel Foerster <pydsigner@gmail.com>
2026-04-24 00:42:58 -0500

    Don't embed CSS in standalone gallery

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,47 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-    entry: {
-        'imageGallery': {
-            import: './src/index.js',
-            filename: 'bundle.js'
+  entry: {
+    imageGallery: {
+      import: "./src/index.js",
+      filename: "bundle.js",
+      layer: "main",
+    },
+    galleryWidget: {
+      import: "./src/gallery-grid.js",
+      filename: "gallery.js",
+      layer: "no-inject",
+    },
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            targets: "defaults",
+            presets: [["@babel/preset-env"]],
+          },
         },
-        'galleryWidget': {
-            import: './src/gallery-grid.js',
-            filename: 'gallery.js'
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        targets: "defaults",
-                        presets: [
-                            ['@babel/preset-env']
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'less-loader'
-                ]
-            },
-        ]
-    },
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        library: '[name]',
-        libraryTarget: 'umd'
-    }
+      },
+      {
+        test: /\.less$/,
+        issuerLayer: "main",
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.less$/,
+        issuerLayer: "no-inject",
+        use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
+      },
+    ],
+  },
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    library: "[name]",
+    libraryTarget: "umd",
+  },
 };
